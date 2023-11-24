c311b18ba62950d50cee86a89167940cd58a173f
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-11-24 16:18:39 +0600

    swagger: use swagger seperate endpoint
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("/", app, document);
+  SwaggerModule.setup("swagger", app, document);
 
   await app.listen(port);
 
