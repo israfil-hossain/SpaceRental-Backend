7aeba677172139b2b8fa217a61833da6dea52dda
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-11-26 23:36:20 +0600

    guard: allow authentication api to be called without guard
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { AuthService } from "./auth.service";
 import { SignInDto } from "./dto/sign-in.dto";
+import { IsPublic } from "./guard/auth.guard";
 
 @ApiTags("Authentication")
 @Controller("Auth")
@@ -9,6 +10,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("SignIn")
+  @IsPublic()
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.email, signInDto.password);
   }
--- src/module/auth/guard/auth.guard.ts
+++ src/module/auth/guard/auth.guard.ts
@@ -1,22 +1,39 @@
+import type { Request } from "express";
+
 import {
   CanActivate,
   ExecutionContext,
   Injectable,
+  SetMetadata,
   UnauthorizedException,
 } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { APP_GUARD } from "@nestjs/core";
+import { APP_GUARD, Reflector } from "@nestjs/core";
 import { JwtService } from "@nestjs/jwt";
-import { Request } from "express";
+
+const IS_PUBLIC_KEY = "isPublic";
+export const IsPublic = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 @Injectable()
 class AuthGuard implements CanActivate {
   constructor(
-    private jwtService: JwtService,
     private configService: ConfigService,
+    private jwtService: JwtService,
+    private reflector: Reflector,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    //#region Allow when IsPublic is used
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+    //#endregion
+
+    //#region Verify jwt token from request or throw
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
@@ -33,8 +50,10 @@ class AuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     return true;
+    //#endregion
   }
 
+  //#region Private helper methods
   private extractTokenFromHeader(request: Request): string {
     const [type, token] = request?.headers?.authorization?.split(" ") ?? [];
     if (type !== "Bearer" || !token) {
@@ -43,6 +62,7 @@ class AuthGuard implements CanActivate {
 
     return token;
   }
+  //#endregion
 }
 
 export const AuthGuardProvider = {
